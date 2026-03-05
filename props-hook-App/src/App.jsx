7e973a6bd533d04d0416448296dd187aa65e6c7e
Sagar0721 <Sagar963963@gmail.com>
2026-03-05 13:04:54 +0530

    props

---
+++ props-hook-App/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Location from "./Location";
+
+export default function App() {
+
+
+  const [counter, setCounter] = useState(1);
+
+  const increment = () => {
+    if (counter < 17) {
+      setCounter(counter + 1);
+    } else {
+      alert("Out of stock..");
+    }
+  }
+
+
+  let myBtnStyle = {
+    backgroundColor: 'orange',
+    border: 'none',
+    margin: '5px',
+    borderRadius: '50px',
+    color: 'white'
+  };
+
+  return <>
+    <center>
+      <h2>Counter App</h2>
+
+      <div>
+        <button style={myBtnStyle} onClick={() => { if (counter > 1) { setCounter(counter - 1) } }}>➖</button>
+        {counter}
+        <button style={myBtnStyle} onClick={increment}>➕</button>
+      </div>
+
+      <Location city="Surat" state="GUJARAT" country="India" num={counter} />
+    </center >
+  </>
+}
---
+++ props-hook-App/src/location.jsx
@@ -0,0 +1,14 @@
+
+export default function location(city,state,country,num){
+
+    return <>
+    <h1>This is location Page </h1>
+
+    <p>city :{city}</p>
+    <p>state :{state}</p>
+    <p>country :{country}</p>
+
+     <h2>{num}</h2>
+
+    </>
+}
